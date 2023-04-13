7367583486dd9307d71d1ddd54534dc8c5bf8376
Vincent <thisoneis4business@gmail.com>
2023-04-13 15:17:18 +1200

    chore: Updated index.ts

--- src/seraph.ts
+++ src/seraph.ts
@@ -6,8 +6,8 @@
 //
 
 import { hydrate, load } from "./ssr";
-import { all, from, memo, query, state, zip } from "./state";
-import { html, mount, render, use } from "./ui";
+import { all, effect, from, memo, query, state, zip } from "./state";
+import { html, modify, mount, render, use } from "./ui";
 
 const std = {
   state,
@@ -21,25 +21,26 @@ const std = {
   mount,
   use,
   render,
+  effect,
+  modify,
 } as const;
 
 export const sr = new Proxy({} as typeof html & typeof std, {
   get(_, key: keyof typeof std | keyof typeof html) {
-    if (
-      key === "state" ||
-      key === "all" ||
-      key === "zip" ||
-      key === "memo" ||
-      key === "from" ||
-      key === "query" ||
-      key === "hydrate" ||
-      key === "load" ||
-      key === "mount" ||
-      key === "use" ||
-      key === "render"
-    ) {
-      return std[key];
-    }
+    if (key === "state") return state;
+    if (key === "all") return all;
+    if (key === "zip") return zip;
+    if (key === "memo") return memo;
+    if (key === "from") return from;
+    if (key === "query") return query;
+    if (key === "hydrate") return hydrate;
+    if (key === "load") return load;
+    if (key === "mount") return mount;
+    if (key === "use") return use;
+    if (key === "render") return render;
+    if (key === "effect") return effect;
+    if (key === "modify") return modify;
+
     return html[key];
   },
 });
