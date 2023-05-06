7b0c8cbada3a12de6d745e47f547ff80725a8170
Vincent <thisoneis4business@gmail.com>
2023-05-07 01:17:52 +1200

    fix: removal of sr namespace

--- src/seraph.ts
+++ src/seraph.ts
@@ -5,48 +5,6 @@
 //  Created by d-exclaimation on 11 Apr 2023
 //
 
-import { load, resource } from "./ssr";
-import { all, effect, from, memo, mutable, query, state, zip } from "./state";
-import { html, hydrate, mount, render, use } from "./ui";
-
-const std = {
-  state,
-  all,
-  zip,
-  memo,
-  from,
-  query,
-  mount,
-  use,
-  render,
-  effect,
-  hydrate,
-  load,
-  resource,
-  mutable,
-} as const;
-
-export const sr = new Proxy({} as typeof html & typeof std, {
-  get(_, key: keyof typeof std | keyof typeof html) {
-    if (key === "state") return state;
-    if (key === "all") return all;
-    if (key === "zip") return zip;
-    if (key === "memo") return memo;
-    if (key === "from") return from;
-    if (key === "query") return query;
-    if (key === "load") return load;
-    if (key === "mount") return mount;
-    if (key === "use") return use;
-    if (key === "render") return render;
-    if (key === "effect") return effect;
-    if (key === "hydrate") return hydrate;
-    if (key === "resource") return resource;
-    if (key === "mutable") return mutable;
-
-    return html[key];
-  },
-});
-
 export * from "./ssr";
 export * from "./state";
 export * from "./ui";
