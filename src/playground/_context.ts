d2eb8e81b32b2823a749c97ca2fd3badce43b3b6
Vincent <thisoneis4business@gmail.com>
2023-06-18 20:55:01 +1200

    feat: Playaround on playground

--- src/playground/_context.ts
+++ src/playground/_context.ts
@@ -2,4 +2,29 @@ import { state } from "@lib/core";
 import { routing } from "@lib/router";
 
 export const $count = state(0);
+export const $analytics = state(true);
+export const $functional = state(false);
+export const $performance = state(false);
 export const router = routing.browser();
+
+declare global {
+  interface Window {
+    __PLAYGROUND__: {
+      $count: typeof $count;
+      $analytics: typeof $analytics;
+      $functional: typeof $functional;
+      $performance: typeof $performance;
+      router: typeof router;
+    };
+  }
+}
+
+if (window) {
+  window.__PLAYGROUND__ = {
+    $count,
+    $analytics,
+    $functional,
+    $performance,
+    router,
+  };
+}
